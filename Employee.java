1ded16ee9a7b74ad1db6a6e44141a579dca4e43a
pavitrapatil2017 <pavikp4@gmail.com>
2023-01-20 11:27:51 +0530

    employee code is added pls check

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Sample
+{
+
+}
+
+class Employee
+{
+public static void main(String[] a)//void main(char*[] , int len)
+{
+System.out.println("Hello Friends welcome to the session");
+}
+}
